b3b3b584d168d0cfae14b1e242e569ebe9b38aa9
ME <architmathur2011@gmail.com>
2018-02-03 09:02:42 +0530

    no processing problem now

--- laugh_detector.py
+++ laugh_detector.py
@@ -1,7 +1,14 @@
+"""
+	Reference - Primary Detection Methods for Laugh Tracks by Keith Wilhelm
+
+"""
+
 import os, sys
 import numpy as np
 from scipy import signal
 from moviepy.editor import VideoFileClip, concatenate_videoclips
+
+import matplotlib.pyplot as plt
 
 file = sys.argv[1].strip()
 if not os.path.isfile(file):
@@ -9,67 +16,63 @@ if not os.path.isfile(file):
 	sys.exit()
 
 file = VideoFileClip(file)
+file.set_duration(int(file.duration))
 file = file.resize((320, 240))
 file_audio = file.audio
+duration = int(file.duration)
 
-clips = []
-num_clips = 5
-clip_size = file.duration//num_clips
+avg_of_10ms=lambda i,f:np.sqrt((f.subclip(i, i+0.01).to_soundarray()**2).mean()) 
+sig = [avg_of_10ms(i, file_audio) for i in np.arange(0.0, duration//1, 0.01)]
 
-for i in range(0, num_clips):
-	if i==num_clips-1:
-		clips.append(file_audio.subclip(i*clip_size, file_audio.duration))
-	else:
-		clips.append(file_audio.subclip(i*clip_size, (i+1)*clip_size))
+sig_len = len(sig)
 
-sigs = [i.to_soundarray() for i in clips]
-for i in range(0, num_clips):
-	sigs[i] = [np.sqrt((a**2).mean()) for a in sigs[i]]
+freq = sig_len/int(file.duration)+0.0
+t = np.arange(0, sig_len, 1)/freq
 
-hil_sigs = [signal.hilbert(i) for i in sigs]
-sig_env = [np.abs(i) for i in hil_sigs]
+print("phasr")
+
+hil_sig = signal.hilbert(sig)
+
+sig_env = np.abs(hil_sig)
+
+avg = np.mean(sig_env)
+print(avg)
+norm_env = [i/avg for i in sig_env]
 
 b, a = signal.butter(3, 0.05)
-smooth_env = [signal.filtfilt(b, a, i) for i in sigs]
+smooth_env = signal.filtfilt(b, a, norm_env)
 
-freq = len(sigs[0])/clips[0].duration
-t = [1000*np.arange(0, len(sigs[0]), 1) / freq, 1000*np.arange(0, len(sigs[-1]), 1) / freq]
+plt.plot(t, smooth_env)
+plt.show()
 
-# print("phase 1")
+#will find better ways to get this; although it works well
+high = 1.8
+low = 0.45
 
-#experiment
-high = 0.2
-low = 0.1
+joke_times, clip, offset, i = [{'s':0, 'e':0}], 0, 0, 0
+while i < sig_len:
+	if smooth_env[i] >= high :
+		
+		if joke_times[-1]['e'] >= t[i]-5:
+			joke_times[-1]['e'] = np.min([t[i]+2, duration])
+		else :
+			joke_times.append({
+				's' : np.max([0, t[i]-2]),
+				'e' : np.min([t[i]+2, duration])
+			})
+			
+		while i < sig_len and smooth_env[i] >= low:
+			joke_times[-1]['e'] = np.min([t[i]+2, duration])
+			i+=1
 
-l, clip, offset = [{'s':0, 'e':0}], 0, 0
-for clip in range(0, num_clips):
-	i = 0
-	while i < len(smooth_env[clip]):
-		# t[clip//4][i] += offset
-		if smooth_env[clip][i] >= high :
-			if l[-1]['e'] >= t[clip//4][i]-5000+offset:
-				l[-1]['e'] = t[clip//4][i]+offset
-			else :
-				l.append({
-					's' : t[clip//4][i]+offset,
-					'e' : t[clip//4][i]+offset
-				})
-				
-			while i < len(smooth_env[clip]) and smooth_env[clip][i] >= low:
-				l[-1]['e'] = t[clip//4][i]+offset
-				i+=1
+	i+=1
 
-				# t[clip//4][i] += offset
-		i+=1
 
-	offset+=clips[clip].duration*1000
-
-# print("phase 1")
-# print(l)
+# print(joke_times)
 
 jokes = []
-for obj in l:
-	jokes.append(file.subclip(obj['s']/1000, obj['e']/1000))
+for obj in joke_times:
+	jokes.append(file.subclip(obj['s'], obj['e']))
 
 jokes = concatenate_videoclips(jokes)
-jokes.write_videofile("jokes_in_" + file.filename, codec='libx264')
+jokes.write_videofile("jokes_in_"+file.filename, codec='libx264')
